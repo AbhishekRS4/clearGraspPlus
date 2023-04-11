89d7cc60dc20ea586d3405765b8e7eb675e386a7
abhishekrs4 <abhishek.r.satyanarayana.4@gmail.com>
2023-04-11 18:12:50 +0200

    added documentation for various functions

--- dataset_capture_kinect_v1/load_kinect_rgbd_images.py
+++ dataset_capture_kinect_v1/load_kinect_rgbd_images.py
@@ -2,6 +2,20 @@ import pickle
 import numpy as np
 
 def load_images_from_pickle(file_pickle):
+    """
+    A generator function that yields a list of images from the given pickle file
+
+    ---------
+    Arguments
+    ---------
+    file_pickle: str
+        full path to the pickle file with images
+
+    ------
+    Yields
+    ------
+        a generator for the images in the pickle file
+    """
     with open(file_pickle, "rb") as file_handler:
         while True:
             try:
@@ -11,9 +25,37 @@ def load_images_from_pickle(file_pickle):
     return
 
 def get_color_images(file_pickle_color):
+    """
+    Function to get a list of color images from the saved pickle file
+
+    ---------
+    Arguments
+    ---------
+    file_pickle_color: str
+        full path to the pickle file with color images
+
+    -------
+    Returns
+    -------
+    color_images: A list of color images loaded from the pickle file as numpy arrays
+    """
     color_images = list(load_images_from_pickle(file_pickle_color))
     return color_images
 
 def get_depth_images(file_pickle_depth):
+    """
+    Function to get a list of depth images from the saved pickle file
+
+    ---------
+    Arguments
+    ---------
+    file_pickle_depth: str
+        full path to the pickle file with depth images
+
+    -------
+    Returns
+    -------
+    depth_images: A list of depth images loaded from the pickle file as numpy arrays
+    """
     depth_images = list(load_images_from_pickle(file_pickle_depth))
     return depth_images
