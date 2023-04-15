39efa15e1d5e22048f93e67b974a3ea816bb6c9d
abhishekrs4 <abhishek.r.satyanarayana.4@gmail.com>
2023-04-15 17:12:45 +0200

    added drn_psa model [surface_normals]

--- pytorch_networks/surface_normals/modeling/aspp.py
+++ pytorch_networks/surface_normals/modeling/aspp.py
@@ -34,7 +34,7 @@ class _ASPPModule(nn.Module):
 class ASPP(nn.Module):
     def __init__(self, backbone, output_stride, BatchNorm):
         super(ASPP, self).__init__()
-        if backbone == 'drn':
+        if backbone == 'drn' or backbone == 'drn_psa':
             inplanes = 512
         elif backbone == 'mobilenet':
             inplanes = 320
