5292f34d3a66ac9893c6ec88092415b4387e4e1d
abhishekrs4 <abhishek.r.satyanarayana.4@gmail.com>
2023-04-24 14:48:49 +0200

    minor fixes to the compute_metrics function [object_segmentation]

--- pytorch_networks/object_segmentation/utils/utils.py
+++ pytorch_networks/object_segmentation/utils/utils.py
@@ -190,12 +190,12 @@ def compute_metrics(pred, label):
     """Compute metrics like True/False Positive, True/False Negative.`
         MUST HAVE ONLY 2 CLASSES: BACKGROUND, OBJECT.
     Args:
-        pred (numpy.ndarray): Prediction, one-hot encoded. Shape: [2, H, W], dtype: uint8
+        pred (numpy.ndarray): Prediction, one-hot encoded. Shape: [H, W], dtype: uint8
         label (numpy.ndarray): Ground Truth, one-hot encoded. Shape: [H, W], dtype: uint8
     Returns:
         float: IOU, TP, TN, FP, FN
     """
-    if len(pred.shape) > 3:
+    if len(pred.shape) > 2:
         raise ValueError("pred should have shape [2, H, W], got: {}".format(pred.shape))
     if len(label.shape) > 2:
         raise ValueError("label should have shape [H, W], got: {}".format(label.shape))
@@ -204,7 +204,7 @@ def compute_metrics(pred, label):
 
     pred = pred.numpy().astype(np.uint8)
     label = label.numpy().astype(np.uint8)
-    total_pixels = pred.shape[0] * pred.shape[1]
+    total_pixels = label.shape[0] * label.shape[1]
 
     tp = np.sum(np.logical_and(pred == 1, label == 1))
     tn = np.sum(np.logical_and(pred == 0, label == 0))
