dcd587b64a787eb0cfae9f6af1599a986fc7564d
abhishekrs4 <abhishek.r.satyanarayana.4@gmail.com>
2023-08-17 19:18:34 +0200

    updated code for resnet34_psa [surface_normals]

--- pytorch_networks/surface_normals/modeling/deeplab.py
+++ pytorch_networks/surface_normals/modeling/deeplab.py
@@ -10,7 +10,7 @@ class DeepLab(nn.Module):
     def __init__(self, backbone='resnet', output_stride=16, num_classes=21,
                  sync_bn=True, freeze_bn=False):
         super(DeepLab, self).__init__()
-        if backbone == 'drn' or backbone == 'drn_psa':
+        if backbone == 'drn' or backbone == 'drn_psa' or backbone == 'resnet34_psa':
             output_stride = 8
 
         if sync_bn == True:
